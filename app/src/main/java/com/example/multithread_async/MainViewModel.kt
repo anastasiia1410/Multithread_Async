c68e9523707baedc04c0173f99c6aaa116a116cc
anastasiia1410 <ar.galkina99@gmail.com>
2023-10-11 10:05:56 +0200

    fixed errors

--- app/src/main/java/com/example/multithread_async/MainViewModel.kt
+++ app/src/main/java/com/example/multithread_async/MainViewModel.kt
@@ -6,25 +6,38 @@ import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
     val randomNumLD = MutableLiveData<List<Int>>()
+    private var thread: Thread? = null
+    private var isThreadRunning: Boolean = false
 
     init {
         getRandomNumber()
     }
 
     private fun getRandomNumber() {
-        Thread {
-            while (true) {
-                val numberList = randomNumLD.value?.toMutableList() ?: mutableListOf()
-                val number = Random.nextInt(1000)
-                numberList.add(number)
-                randomNumLD.postValue(numberList)
+        if (!isThreadRunning) {
+            thread = Thread {
+                while (isThreadRunning) {
+                    val numberList = randomNumLD.value?.toMutableList() ?: mutableListOf()
+                    val number = Random.nextInt(1000)
+                    numberList.add(number)
+                    randomNumLD.postValue(numberList)
 
-                try {
-                    Thread.sleep(1000)
-                } catch (e: InterruptedException) {
-                    return@Thread
+                    try {
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                        return@Thread
+                    }
                 }
             }
-        }.start()
+            thread?.start()
+            isThreadRunning = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        thread?.interrupt()
+        isThreadRunning = false
+
     }
 }
