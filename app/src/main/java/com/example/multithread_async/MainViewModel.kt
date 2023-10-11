b9351f2f100b7973eb63ac9ff1b43c46250ebdc1
anastasiia1410 <ar.galkina99@gmail.com>
2023-10-11 09:32:32 +0200

    fixed rxJava code

--- app/src/main/java/com/example/multithread_async/MainViewModel.kt
+++ app/src/main/java/com/example/multithread_async/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -19,6 +20,7 @@ class MainViewModel : ViewModel() {
     private fun getRandomNumber() {
         val disposable = Observable.interval(1, TimeUnit.SECONDS)
             .map { Random.nextInt(1000) }
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { numbers ->
                 val numberList = randomNumLD.value?.toMutableList() ?: mutableListOf()
